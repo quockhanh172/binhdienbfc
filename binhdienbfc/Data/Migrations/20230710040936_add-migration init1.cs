b48ea91ad8b5f56a858f9db403944438e3dbf2e0
quockhanh172 <101686329+quockhanh172@users.noreply.github.com>
2023-07-12 01:09:08 -0700

    hello world

---
+++ binhdienbfc/Data/Migrations/20230710040936_add-migration init1.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace binhdienbfc.Data.Migrations
+{
+    public partial class addmigrationinit1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ binhdienbfc/Data/Product.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace binhdienbfc.Data
+{
+    [Table("Product")]
+    public class Product
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+    }
+}
